8f0e0e8e23f84006445a3f84341ee2878467e7ed
Jay Modi <89065688+jaymodi993@users.noreply.github.com>
2024-04-09 14:52:09 +0530

    i

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed("#element", {
-  strings: ["Web Developer", "Programmer", "App Developer"],
+  strings: ["Web Developer", "Programmer", "Freelancer"],
   typeSpeed: 100,
   loop: true,
   loopCount: Infinity,
