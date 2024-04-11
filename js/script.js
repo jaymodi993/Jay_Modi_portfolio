2634b412d51304068477aaa1c77219124e0d2b9d
Jay Modi <jaymodi993@gmail.com>
2024-04-11 21:21:39 +0530

    responsive

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ var typed = new Typed("#element", {
   loop: true,
   loopCount: Infinity,
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const right = document.querySelector(".right");
+  const navbarLinks = document.querySelector(".navbar-links");
+
+  right.addEventListener("click", function () {
+    navbarLinks.classList.toggle("active");
+  });
+  navbarLinks.addEventListener("click", function () {
+    navbarLinks.classList.remove("active");
+  });
+});
